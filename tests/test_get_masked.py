0ad338c91d53bb043c2ffe34c706fd3758db5052
Valera Demin <wahrwolffe@gmail.com>
2023-09-10 02:02:30 +0300

    test get_masked

---
+++ tests/test_get_masked.py
@@ -0,0 +1,8 @@
+from utils.functions import get_masked
+
+
+def test_get_masked():
+    assert get_masked(None) == "Открытие вклада"
+    assert get_masked("Счет 01234567890123456789") == "Счет **6789"
+    assert get_masked("Maestro 7810846596785568") == "Maestro 7810 84** **** 5568"
+    assert get_masked("Visa Classic 2842878893689012") == "Visa Classic 2842 87** **** 9012"
